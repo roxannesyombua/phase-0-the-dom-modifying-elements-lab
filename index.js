2d7525a7bde8e8834a0d7aa410621938290e3866
roxannesyombua <roxannesyombua@gmail.com>
2024-04-12 07:34:50 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main")
+main.remove();
+
+let newHeader = document.createElement("h1")
+
+newHeader.setAttribute("id", "victory")
+
+newHeader.innerHTML = "Amanda is the champion"
